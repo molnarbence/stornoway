8f2792698d27ee0dd51fb87cd59bfaf3e6b10123
Bence Molnár <xjarekx@gmail.com>
2025-09-21 20:30:31 +0200

    First attempt

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  nitro: {
+    preset: 'static',
+  },
+  ssr: true,
+
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: ['@nuxt/image', '@nuxt/eslint'],
+})
