014cad39b89fc13f06ebf1ec44b8a3ae7ac37997
Bence Molnár <xjarekx@gmail.com>
2025-09-23 22:48:56 +0200

    Fix format

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,11 +2,12 @@
 
 <template>
   <div class="mx-auto max-w-5xl lg:max-w-6xl px-4 sm:px-6 lg:px-10 xl:px-14 2xl:px-20 py-8">
-    <div class="w-full rounded-xl border border-gray-200 dark:border-gray-800 bg-white dark:bg-neutral-900 shadow-sm p-6 sm:p-8 lg:p-10 xl:p-12">
+    <div
+      class="w-full rounded-xl border border-gray-200 dark:border-gray-800 bg-white dark:bg-neutral-900 shadow-sm p-6 sm:p-8 lg:p-10 xl:p-12"
+    >
       <slot />
     </div>
   </div>
-  
 </template>
 
 <style scoped></style>
